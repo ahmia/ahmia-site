cb3816bda732f245898a97087ad85f4a0765577e
Juha Nurmi <juha.nurmi@ahmia.fi>
2014-06-04 14:14:57 +0300

    Django version updated from 1.4 to 1.6: resolving relative path problems with Apache WSGI.

--- settings.py
+++ settings.py
@@ -137,7 +137,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
-    #'ahmia',
+    'ahmia',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
