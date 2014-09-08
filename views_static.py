81ea96e0911cf6a8885a03ddfc57d87c87687c97
Juha Nurmi <juha.nurmi@ahmia.fi>
2014-09-08 13:33:13 +0300

    Documentation fixes. Policy template to show only those MD5 sums that are from services that are online.

--- views_static.py
+++ views_static.py
@@ -6,66 +6,62 @@ These pages does not require database connection.
 
 """
 from django.http import HttpResponse, HttpResponseNotAllowed
+from django.template import Context, loader
+from django.views.decorators.http import require_GET
 
 import view_help_functions as helpers  # My view_help_functions.py
+from ahmia.models import HiddenWebsite
 
 
+@require_GET
 def indexing(request):
     """Static page about the indexing and crawling."""
-    if request.method == 'GET':
-        return helpers.render_page('indexing.html')
-    return HttpResponseNotAllowed("Only GET request is allowed.")
+    return helpers.render_page('indexing.html')
 
+@require_GET
 def policy(request):
     """Static policy page."""
-    if request.method == 'GET':
-        return helpers.render_page('policy.html', show_descriptions=True)
-    return HttpResponseNotAllowed("Only GET request is allowed.")
+    template = loader.get_template("policy.html")
+    onions = HiddenWebsite.objects.all()
+    count_online = onions.filter(banned=False, online=True).count()
+    count_banned = onions.filter(banned=True, online=True).count()
+    content = Context({'onions': onions, 'count_banned': count_banned,
+    'count_online': count_online})
+    return HttpResponse(template.render(content))
 
+@require_GET
 def disclaimer(request):
     """Static disclaimer page."""
-    if request.method == 'GET':
-        return helpers.render_page('disclaimer.html')
-    return HttpResponseNotAllowed("Only GET request is allowed.")
+    return helpers.render_page('disclaimer.html')
 
+@require_GET
 def description_proposal(request):
     """Static description proposal."""
-    if request.method == 'GET':
-        return helpers.render_page('descriptionProposal.html')
-    return HttpResponseNotAllowed("Only GET request is allowed.")
+    return helpers.render_page('descriptionProposal.html')
 
+@require_GET
 def create_description(request):
     """Page to create hidden website description."""
-    if request.method == 'GET':
-        return helpers.render_page('createHsDescription.html')
-    else:
-        return HttpResponseNotAllowed("Only GET request is allowed.")
+    return helpers.render_page('createHsDescription.html')
+    return HttpResponseNotAllowed("Only GET request is allowed.")
 
+@require_GET
 def documentation(request):
     """Static documentation page."""
-    if request.method == 'GET':
-        return helpers.render_page('documentation.html')
-    else:
-        return HttpResponseNotAllowed("Only GET request is allowed.")
+    return helpers.render_page('documentation.html')
 
+@require_GET
 def about(request):
     """Static about page of ahmia."""
-    if request.method == 'GET':
-        return helpers.render_page('about.html')
-    else:
-        return HttpResponseNotAllowed("Only GET request is allowed.")
+    return helpers.render_page('about.html')
 
+@require_GET
 def gsoc(request):
     """Static Google Summer of Code 2014 proposal page."""
-    if request.method == 'GET':
-        return helpers.render_page('gsoc.html')
-    else:
-        return HttpResponseNotAllowed("Only GET request is allowed.")
+    return helpers.render_page('gsoc.html')
 
+@require_GET
 def show_ip(request):
     """Returns the IP address of the request."""
-    if request.method == 'GET':
-        ip_addr = helpers.get_client_ip(request)
-        return HttpResponse(ip_addr)
-    else:
-        return HttpResponseNotAllowed("Only GET request is allowed.")
+    ip_addr = helpers.get_client_ip(request)
+    return HttpResponse(ip_addr)
