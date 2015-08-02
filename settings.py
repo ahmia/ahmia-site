1aa77bff82166d3e16ff7e37759c4d5926b4c255
razorfinger <razorfinger@users.noreply.github.com>
2015-08-01 22:12:10 -0400

    mmm clean

--- settings.py
+++ settings.py
@@ -134,7 +134,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sites',
     'django.contrib.staticfiles',
-    'debug_toolbar',
+    #'debug_toolbar',
     'ahmia',
     'haystack',
 )
