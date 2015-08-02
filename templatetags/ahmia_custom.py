046f0f4ffd6e2aaf5740dee6c84b1de95284a64d
razorfinger <razorfinger@users.noreply.github.com>
2015-08-01 23:14:15 -0400

    add custom template tags

---
+++ templatetags/ahmia_custom.py
@@ -0,0 +1,15 @@
+from django import template
+from django.conf import settings
+
+register = template.Library()
+
+@register.assignment_tag
+def is_development_environment():
+  return settings.DEBUG == True
+
+@register.simple_tag
+def get_environment():
+    if settings.DEBUG == True:
+        return 'development'
+    else:
+        return 'production'
