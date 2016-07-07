fd074e55c57b62c966732c11af3cf7e6c7de38e6
Ismael R <ismael@zma.fr>
2016-07-07 17:56:56 +0200

    Clean ahmia module

--- ahmia/ahmia/validators.py
+++ ahmia/ahmia/validators.py
@@ -5,30 +5,25 @@ import re
 from django.core.exceptions import ValidationError, ObjectDoesNotExist
 from django.utils.translation import ugettext as _
 
-from .models import HiddenWebsite
-
 def validate_status(value):
     """Test if an onion domain is not banned."""
     try:
-        onion = HiddenWebsite.objects.get(id=value)
-        if onion.banned:
-            raise ValidationError(
-                _("This onion is banned and cannot be added to this index.")
-            )
+        '''raise ValidationError(
+            _("This onion is banned and cannot be added to this index.")
+        )'''
+        return
     except ObjectDoesNotExist:
         pass
 
 def validate_onion_url(url):
     """ Test is url correct onion URL."""
     #Must be like http://3g2upl4pq6kufc4m.onion/
-    if len(url) != 30:
-        raise ValidationError(u'%s length is not 30' % url)
     if url[0:7] != 'http://':
-        raise ValidationError(u'%s is not beginning with http://' % url)
+        raise ValidationError(_(u'{url} is not beginning with http://', url=url))
     if url[-7:] != '.onion/':
-        raise ValidationError(u'%s is not ending with .onion/' % url)
-    if not re.match("[a-z2-7]{16}", url[7:-7]):
-        raise ValidationError(u'%s is not valid onion domain' % url)
+        raise ValidationError(_(u'{url} is not ending with .onion/', url=url))
+    if not validate_onion(url[7:-7]):
+        raise ValidationError(_(u'{url} is not valid onion domain', url=url))
 
 def validate_onion(url):
     """Test if an url is a valid hiddenservice url"""
