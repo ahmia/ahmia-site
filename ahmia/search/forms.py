7bf388f8bf9d8e240c17d36d6070e5f50f4735b8
Ismael R <ismael@zma.fr>
2016-07-29 03:12:28 +0200

    Update search app to make it work with new index

--- ahmia/search/forms.py
+++ ahmia/search/forms.py
@@ -1,9 +1,12 @@
 """Forms used in Ahmia."""
 
+from django import forms
+from django.utils.translation import ugettext as _
+
 class SearchForm(forms.Form):
     query = forms.CharField(
         widget=forms.TextInput(
             attrs={'placeholder': _('Enter your .onion address here')}
         )
     )
-    page = forms.HiddenField()
+    page = forms.IntegerField()
--- ahmia/search/views.py
+++ ahmia/search/views.py
@@ -4,18 +4,12 @@ Full text search views.
 """
 import math
 import time
-from datetime import date
-import urllib # URL encoding
-import re
+from datetime import date, datetime
 
 import urllib3
 import simplejson as json
 
-from django.conf import settings  # For the back-end connection settings
-from django.http import HttpResponse
-from django.shortcuts import redirect
-from django.template import Context, loader
-from django.views.decorators.http import require_GET, require_http_methods
+from django.views.generic.base import TemplateView, RedirectView
 
 class OnionRedirectView(RedirectView):
     permanent = False
@@ -25,10 +19,14 @@ class OnionRedirectView(RedirectView):
     def update_stat_counter(self):
         pass
 
+    def get(self, request, *args, **kwargs):
+        kwargs['redirect_url'] = request.GET.get('redirect_url', '')
+        return super(OnionRedirectView, self).get(*args, **kwargs)
+
     def get_redirect_url(self, *args, **kwargs):
-        redirect_url = request.GET.get('redirect_url', '')
+        redirect_url = kwargs['redirect_url']
         if not redirect_url:
-            answer = "Bad request: no GET parameter URL."
+            #answer = "Bad request: no GET parameter URL."
             return None
         self.update_stat_counter()
         return redirect_url
@@ -36,7 +34,7 @@ class OnionRedirectView(RedirectView):
 class TorResultsView(TemplateView):
     http_method_names = ['get']
     template_name = "tor_results.html"
-    RESULTS_PER_PAGE = 50
+    RESULTS_PER_PAGE = 5
 
     def get_pool(self):
         #pool = urllib3.HTTPSConnectionPool(settings.ELASTICSEARCH_HOST,
@@ -45,30 +43,96 @@ class TorResultsView(TemplateView):
         pool = urllib3.HTTPConnectionPool('127.0.0.1', '9200')
         # For testing, external Elasticsearch end-point
         #pool = urllib3.HTTPSConnectionPool('ahmia.fi', '443')
+        return pool
 
     def get_endpoint(self, query, page):
         # For testing, external Elasticsearch end-point
         # endpoint =
-        #    '/elasticsearch/crawl/' + item_type + '/_search/?size=100&q=' + query
-        query = urllib.quote_plus(re.escape(query).encode('utf-8'))
-        return '/crawl/tor/_search/?size=100&q=' + query
+        #    '/elasticsearch/crawl/' + item_type + '/_search/?size=100&q=' + q
+        #query = urllib.quote_plus(re.escape(query).encode('utf-8'))
+        return '/crawl/tor/_search/'
+
+    def get_data(self, query, page):
+        return json.dumps({
+            "query": {
+                "function_score": {
+                    "functions": [
+                        {
+                            "field_value_factor": {
+                                "field": "authority",
+                                "modifier": "log1p",
+                                "factor": 100000
+                            }
+                        }
+                    ],
+                    "query": {
+                        "bool": {
+                            "should": [
+                                {
+                                    "multi_match": {
+                                        "query": query,
+                                        "type":   "most_fields",
+                                        "fields": [
+                                            "fancy",
+                                            "fancy.stemmed",
+                                            "fancy.shingles"
+                                        ],
+                                        "boost": 2,
+                                        "minimum_should_match": "75%",
+                                        "cutoff_frequency": 0.01
+                                    }}
+                            ],
+                            "must": [
+                                {
+                                    "multi_match": {
+                                        "query":  query,
+                                        "type":   "most_fields",
+                                        "fields": [
+                                            "content",
+                                            "content.stemmed",
+                                            "content.shingles"
+                                        ],
+                                        "minimum_should_match": "75%",
+                                        "cutoff_frequency": 0.01
+                                    }}
+                            ]
+                        }
+                    }
+                }
+            },
+            "size": self.RESULTS_PER_PAGE,
+            "from": self.RESULTS_PER_PAGE * page,
+            "_source": {
+                "include": ["title", "url", "meta", "updated_on", "domain"]
+            }
+        })
 
     def request_elasticsearch(self, query, page):
         """Return a dict of Elasticsearch results."""
-        try:
-            return self.get_pool().request(
-                'GET',
-                self.get_endpoint(query, page)
-            )
-        except:
-            return None
+        return self.get_pool().request(
+            'GET',
+            self.get_endpoint(query, page),
+            body=self.get_data(query, page)
+        )
 
-    def format_response(self, request):
-        return json.loads(response.data)
+    def format_response(self, response):
+        res_dict = json.loads(response.data)
+        total = res_dict['hits']['total']
+        results = [h['_source'] for h in res_dict['hits']['hits']]
+        for res in results:
+            res['updated_on'] = datetime.strptime(res['updated_on'],
+                                                  '%Y-%m-%dT%H:%M:%S')
+        return total, results
+
+    def get(self, request, *args, **kwargs):
+        kwargs['q'] = request.GET.get('q', '')
+        kwargs['page'] = request.GET.get('page', 0)
+        context = self.get_context_data(**kwargs)
+        return self.render_to_response(context)
 
     def get_context_data(self, **kwargs):
-        query_string = request.GET.get('q', '')
-        page = request.GET.get('page', 0)
+        query_string = kwargs['q']
+        page = kwargs['page']
         search_time = ""
         # assert query_string not None
         start = time.time()
@@ -83,10 +147,10 @@ class TorResultsView(TemplateView):
         max_pages = int(math.ceil(float(length) / self.RESULTS_PER_PAGE))
 
         return {
-            'page': page,
+            'page': page+1,
             'max_pages': max_pages,
-            'result_begin': ,
-            'result_end': ,
+            'result_begin': self.RESULTS_PER_PAGE * page,
+            'result_end': self.RESULTS_PER_PAGE * (page + 1),
             'total_search_results': length,
             'query_string': query_string,
             'search_results': results,
@@ -95,5 +159,5 @@ class TorResultsView(TemplateView):
         }
 
 class IipResultsView(TorResultsView):
-    def get_endpoint(self, query):
+    def get_endpoint(self, query, page):
         return '/crawl/i2p/_search/?size=100&q=' + query
