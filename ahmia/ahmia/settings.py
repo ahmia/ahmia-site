638f5530c9131a147b3a8fc4575048cb59329935
Ismael R <ismael@zma.fr>
2016-08-10 18:58:18 +0200

    Change elasticsearch's settings to the same than the crawler

--- ahmia/ahmia/settings.py
+++ ahmia/ahmia/settings.py
@@ -49,11 +49,9 @@ else:
 # ELASTICSEARCH STUFF
 ELASTICSEARCH_TLS_FPRINT = \
     "8C:DC:67:EA:C3:B3:97:94:92:30:81:35:8C:C6:D9:2A:E2:E6:8E:3E"
-ELASTICSEARCH_HOST = "ahmia.fi"
-ELASTICSEARCH_PORT = 443
-
-PROXY_BASE_URL = 'http://localhost:9200/'
-# For external access 'https://ahmia.fi/elasticsearch/'
+ELASTICSEARCH_SERVERS = 'http://localhost:9200'
+ELASTICSEARCH_INDEX = 'crawl'
+ELASTICSEARCH_TYPE = 'tor'
 
 # Email settings
 EMAIL_USE_TLS = True
