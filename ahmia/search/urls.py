c83b910dea051bb23bd430f6dc0f140a2e29f6ba
Ismael R <ismael@zma.fr>
2016-08-18 18:14:55 +0200

    Removing old disabled links

--- ahmia/search/urls.py
+++ ahmia/search/urls.py
@@ -6,11 +6,4 @@ from . import views
 urlpatterns = [
     url(r'^', views.TorResultsView.as_view(), name="results"), # results
     url(r'^i2p/', views.IipResultsView.as_view(), name="results-i2p"), # results
-    # Redirect link to hidden service
-    url(r'^redirect/', views.OnionRedirectView.as_view(), name="redirect"),
 ]
-
-# Elasticsearch API
-'''urlpatterns += [
-    url(r'^elasticsearch/', views.proxy),
-]'''
