142a661ed638cb4ef28a4bcba3814c5fb32ac085
mdhash <pushkarpathak21@gmail.com>
2017-07-16 19:04:32 +0530

    Stats page upgrades

--- ahmia/stats/views.py
+++ ahmia/stats/views.py
@@ -6,6 +6,14 @@ Statistics: JSON data API and JavaScript viewers.
 """
 from ahmia.views import CoreView
 
+class Stats(CoreView):
+    """Statisics Page"""
+    template_name = "stats.html"
+
+class LinkGraph(CoreView):
+    """Visualizing Linking Graph"""
+    template_name = "link_graph.html"
+
 class OnionsOverTimeView(CoreView):
     """Displays tor2web statitics of .onions over time"""
     template_name = "onions_over_time.html"
