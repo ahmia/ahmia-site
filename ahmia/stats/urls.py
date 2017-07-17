5c3c36fdcd690b327bb27114e79ea1629a00deb0
juhanurmi <juha.nurmi@ahmia.fi>
2017-07-17 10:15:38 +0300

    Fix static paths in onion graph.

--- ahmia/stats/urls.py
+++ ahmia/stats/urls.py
@@ -1,10 +1,20 @@
 """The URL patterns of the ahmia stats app."""
-from django.conf.urls import url
-
+import os
 from . import views
+from django.conf.urls import url
+from django.views.static import serve
 
 urlpatterns = [
     url(r'^$', views.Stats.as_view(), name="stats"),
     url(r'^link_graph/', views.LinkGraph.as_view(), name="link_graph"),
     url(r'^onionsovertime/', views.OnionsOverTimeView.as_view(), name="onionsovertime"),
+]
+
+# static files: CSS, JavaScript, images
+urlpatterns += [
+    url(
+        r'^static/(?P<path>.*)$',
+        serve,
+        {'document_root': os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static')}
+    )
 ]
