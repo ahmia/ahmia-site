dbf2bef08e9ebff6bf47c6f043b63ecf64fb9883
juhanurmi <juha.nurmi@ahmia.fi>
2018-05-16 10:45:11 +0300

    Skeleton code to gather user clicks volumes in Ahmia site.

--- ahmia/search/urls.py
+++ ahmia/search/urls.py
@@ -4,6 +4,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^i2p/', views.IipResultsView.as_view(), name="results-i2p"), # results
-    url(r'^', views.TorResultsView.as_view(), name="results"), # results
+    url(r'^i2p/', views.IipResultsView.as_view(), name="results-i2p"), # Results
+    # Redirect to hidden service
+    url(r'^search/redirect', views.onion_redirect),
+    # This is the last
+    url(r'^', views.TorResultsView.as_view(), name="results"), # Results
 ]
